676587bd6f26a701aac8c3e99e16d856235036cf
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-11-30 21:39:32 +0100

    auth fixing bugs

--- src/server/routes/auth-route.ts
+++ src/server/routes/auth-route.ts
@@ -1,40 +1,41 @@
-import { CredentialsType } from "@/lib/Validation/CredentialsType";
-import { publicProcedure, router } from "../trpc";
-import { getPayloadClient } from "@/lib/getPayloadClient";
-import { TRPCClientError } from "@trpc/client";
-import { TRPCError } from "@trpc/server";
+import { CredentialsType } from '@/lib/Validation/CredentialsType'
+import { publicProcedure, router } from '../trpc'
+import { getPayloadClient } from '@/lib/getPayloadClient'
+import { TRPCClientError } from '@trpc/client'
+import { TRPCError } from '@trpc/server'
 
 export const authRoute = router({
-  createPayloadUser: publicProcedure.input(CredentialsType).mutation(async ({ input, ctx }) => {
-    const { email, password } = input
-  const payload = await getPayloadClient()
-  const{docs}= await payload.find({
-    collection: "users",
-    where: {
-      email:{
-        equals: email
-      },
-      password:{
-       equals:password
-      }
-    },
-    limit: 1
-  })
+  createPayloadUser: publicProcedure
+    .input(CredentialsType)
+    .mutation(async ({ input, ctx }) => {
+      const { email, password } = input
+      const payload = await getPayloadClient()
+      const { docs } = await payload.find({
+        collection: 'users',
+        where: {
+          email: {
+            equals: email,
+          },
+          password: {
+            equals: password,
+          },
+        },
+        limit: 1,
+      })
 
-  if(docs.length>0){
-    throw new TRPCError({
-      code: "CONFLICT",
-      cause: new TRPCClientError("User Already Exists"),
-      message: "User Already Exists",
-    })
-  
-  }
-  await payload.create({
-   collection:"users",
-   data:{
-    email,
-    password
-   }
-  })
-  }),
+      if (docs.length > 0) {
+        throw new TRPCError({
+          code: 'CONFLICT',
+          cause: new TRPCClientError('User Already Exists'),
+          message: 'User Already Exists',
+        })
+      }
+      await payload.create({
+        collection: 'users',
+        data: {
+          email,
+          password,
+        },
+      })
+    }),
 })
